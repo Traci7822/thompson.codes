95a9a98ede1db6689ce03d27511825e4e2e2e6f5
Traci7822 <Tracir@gmail.com>
2019-03-01 16:17:30 -0700

    Banner design

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import {hot} from "react-hot-loader";
 import "../styles/banner.css";
 
+import mountain from '../assets/mountain.png';
+
 class Banner extends Component {
   constructor() {
     super(); // populates this.props (fixed for lifetime of component)
@@ -12,6 +14,11 @@ class Banner extends Component {
   render() {
     return (
       <div className="banner">
+        <span className="title">
+        <h1 className="name">THOMPSON</h1>
+          <img className="title_image" src={mountain} />
+         <h1 className="name">CODES</h1>
+         </span>
       </div>
     )
   }
