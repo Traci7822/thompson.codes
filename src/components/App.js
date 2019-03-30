1ea2a7e9ed788b44c58c1f8e4f616b101b4ea9ad
Traci7822 <Tracir@gmail.com>
2019-03-29 19:23:41 -0600

    Styling changes - in progress

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,83 @@ import "../styles/app.css";
 
 import Banner from './banner'
 import Profiles from './profiles'
+import About from './about'
 
 class App extends Component {
   constructor() {
     super(); // populates this.props (fixed for lifetime of component)
     this.state = { // data that will change using setState
-      results: {}
+      showBanner: true,
+      showAbout: true,
+      showProfiles: true
     }
+    this.displayBanner = this.displayBanner.bind(this);
+    this.hideBanner = this.hideBanner.bind(this);
+    this.displayProfiles = this.displayProfiles.bind(this);
+
   }
+
+  displayBanner() {
+    this.setState({
+      showBanner: true,
+      showAbout: false,
+      showProfiles: false
+    })
+  }
+
+  hideBanner() {
+    this.setState({
+      showBanner: false,
+      showAbout: true,
+      showProfiles: false
+    })
+  }
+
+  displayProfiles() {
+    this.setState({
+      showBanner: false,
+      showAbout: false,
+      showProfiles: true
+    })
+  }
+
   render() {
-    return (
-      <div className="app">
-        <div className="row">
-          <Banner />
-          <Profiles />
+    // Figure out how to actual do conditional rendering
+    if (this.state.showBanner) {
+     return (
+        <div className="app">
+          <div className="row">
+            <button className="banner" onClick={this.hideBanner}>
+              <Banner />
+            </button>
+          </div>
         </div>
-      </div>
-    );
+      )
+    }
+
+    if (this.state.showAbout) {
+      return (
+        <div className="app">
+          <div className="row">
+            <button className="about" onClick={this.displayProfiles}>
+              <About />
+            </button>
+          </div>
+        </div>
+      )
+    }
+
+    if (this.state.showProfiles) {
+      return (
+        <div className="app">
+          <div className="row">
+            <button className="about" onClick={this.displayBanner}>
+              <Profiles />
+            </button>
+          </div>
+        </div>
+      )
+    }
   }
 }
 
