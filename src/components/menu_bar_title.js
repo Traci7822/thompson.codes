bb7d3009c79ff391d5efa6d1a74e1d5062714b50
Bryan Thompson <bryan@thompson.codes>
2019-04-13 14:44:48 -0600

    Adjusted proptypes import to match existing style

--- src/components/menu_bar_title.js
+++ src/components/menu_bar_title.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import {hot} from "react-hot-loader";
 import "../styles/menu_bar.css";
 
