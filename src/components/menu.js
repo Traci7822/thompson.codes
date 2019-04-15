87e59e7b4e2e9e6c0565c97afdc9f4ca3123f9b1
Traci7822 <Tracir@gmail.com>
2019-04-15 09:32:40 -0600

    Setting up for expanding index sections

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,47 +1,33 @@
 import React, { Component } from "react";
 import {hot} from "react-hot-loader";
 import "../styles/menu.css";
-import Details from './details';
 
 import MenuBar from './menu_bar';
 
 class Menu extends Component {
   constructor() {
-    super(); // populates this.props (fixed for lifetime of component)
-    this.state = { // data that will change using setState
-      // showDetails: false,
+    super();
+    this.state = {
+      showDetails: false,
     }
   }
 
-  toggleDetails = () => {
-    this.setState({
-      showDetails: !this.state.showDetails,
-      id: this.state.id,
-      title: this.state.title
-    });
-  }
+  // toggleDetails = () => {
+  //   console.log(this);
+  //   this.setState({
+  //     showDetails: !this.state.showDetails,
+  //     id: this.state.id,
+  //     title: this.state.title
+  //   });
+  // }
 
   render() {
-    // const details = null;
-    let details;
-    const active = this.state.showDetails ? ' active' : '';
-
-    if (this.state.showDetails) {
-      details = <Details key={this.state.title} title={this.state.title} id={this.state.id} showDetails={this.state.showDetails} />;
-    } else {
-      details = '';
-    }
     return (
       <div className="menu_and_details">
         <div className="menu_bar_container">
-          {/* TODO: Make bars dynamic and interactable */}
           {Object.entries(this.props.pages).map(function(page) {
-            return <MenuBar key={page[0]} title={page[1]} id={page[0]} toggleDetails={this.toggleDetails}/>
+            return <MenuBar key={page[0]} title={page[1]} id={page[0]} toggleDetails={this.toggleDetails} showDetails={this.state.showDetails} />
           }, this )}
-        </div>
-        {/* set this over the menubar container */}
-        <div className={"details_container " + active}>
-          {details}
         </div>
       </div>
     )
--- src/components/menu_bar.js
+++ src/components/menu_bar.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import {hot} from "react-hot-loader";
 import "../styles/menu_bar.css";
 import MenuBarTitle from './menu_bar_title';
+import Details from './details';
+
 
 class MenuBar extends Component {
   constructor() {
-    super(); // populates this.props (fixed for lifetime of component)
-    this.state = { // data that will change using setState
-      // showDetails: false,
+    super();
+    this.state = {
     }
   }
 
@@ -17,15 +18,24 @@ class MenuBar extends Component {
       id: this.props.id,
       title: this.props.title
     });
-    this.props.toggleDetails();
   }
 
   render() {
+    let details;
+    let expand = this.state.showDetails ? ' expand' : '';
+    if (this.state.showDetails) {
+      details = <Details key={this.state.title} title={this.state.title} id={this.state.id} showDetails={this.state.showDetails} />;
+    } else {
+      details = '';
+    }
     return (
-      <div className={"menu_bar_row"} onMouseOver={this.toggleDetails} onMouseOut={this.toggleDetails}>
+      <div className={"menu_bar_row" + expand} onClick={this.toggleDetails}>
         <div className={"menu_bar menu_bar_" + this.props.id}>
           <MenuBarTitle title={this.props.title} />
         </div>
+        {/* <div className={"details_container active"}>
+          {details}
+        </div> */}
       </div>
     )
   }
